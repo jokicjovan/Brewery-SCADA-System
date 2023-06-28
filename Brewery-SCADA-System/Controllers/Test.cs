b894e60c69084414f0c44080425101fd3cef9bea
Jovan Jokić <102528502+jokicjovan@users.noreply.github.com>
2023-06-28 14:14:08 +0200

    Swagger added

---
+++ Brewery-SCADA-System/Controllers/Test.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Brewery_SCADA_System.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]/[action]")]
+    public class Test : ControllerBase
+    {
+        public Test()
+        {
+        }
+
+        [HttpGet]
+        public ActionResult Index()
+        {
+            return Ok(0);
+        }
+    }
+}
