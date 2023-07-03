02ceae01242c8f799de8a67b35bde768ebca412e
mrmijan <mijanovic.boki.bojan@gmail.com>
2023-07-03 16:36:01 +0200

    Minor error fixed

--- Backend/Database/DatabaseContext.cs
+++ Backend/Database/DatabaseContext.cs
@@ -17,10 +17,13 @@ namespace Brewery_SCADA_System.Database
             options.UseSqlite(Configuration.GetConnectionString("WebApiDatabase"));
         }
 
-        public DbSet<User> Users { get; set; }
+        public DbSet<Alarm> Alarm { get; set; }
         public DbSet<AnalogInput> AnalogInput { get; set; }
         public DbSet<AnalogOutput> AnalogOutput { get; set; }
         public DbSet<DigitalInput> DigitalInput { get; set; }
         public DbSet<DigitalOutput> DigitalOutput { get; set; }
+        public DbSet<IOAnalogData> IOAnalogData { get; set; }
+        public DbSet<IODigitalData> IODigitalData { get; set; }
+        public DbSet<User> Users { get; set; }
     }
 }
