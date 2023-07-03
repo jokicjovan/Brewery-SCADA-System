b74c0c6d7cd6c93fd7674b7cbd6bca4b90df11c5
Jovan Jokić <102528502+jokicjovan@users.noreply.github.com>
2023-07-03 17:23:53 +0200

    Authentication and authorization front implementation

---
+++ Frontend/src/components/LoginForm.tsx
@@ -0,0 +1,103 @@
+import {useState} from "react";
+import {Avatar, Box, Button, CssBaseline, InputLabel, Stack, TextField, Typography} from "@mui/material";
+import {Link, useNavigate} from "react-router-dom";
+import axios from "axios";
+import {environment} from "./Environment.tsx";
+
+export default function LoginForm(){
+    const [email, setEmail] = useState("")
+    const [password, setPassword] = useState("")
+    const [error, setError] = useState("")
+    const navigate = useNavigate()
+
+
+    function submitHandler(event : any) {
+        event.preventDefault()
+        axios.post(environment + `/api/User/login`, {
+            email: email,
+            password: password
+        }).then(res => {
+            if (res.status === 200){
+                navigate(0);
+            }
+        }).catch((error) => {
+            if (error.response?.status !== undefined && error.response.status === 404){
+                setError("Invalid username or password!");
+            }
+            else if (error.response?.status !== undefined && error.response.status === 400){
+                setError("Invalid input!");
+            }
+            else{
+                setError("An error occurred!");
+            }
+        });
+    }
+
+
+    return <>
+        <CssBaseline />
+        <Box
+            sx={{
+                marginTop: 8,
+                display: 'flex',
+                flexDirection: 'column',
+                alignItems: 'center',
+            }}
+        >
+            <Avatar sx={{ m: 1, bgcolor: 'primary.main' }}>
+
+            </Avatar>
+            <Typography component="h1" variant="h3">
+                Sign in
+            </Typography>
+            <Box component="form" onSubmit={submitHandler} sx={{ mt: 1 }}>
+                <TextField
+                    margin="normal"
+                    required
+                    fullWidth
+                    autoFocus
+                    type="text"
+                    id="email"
+                    label="Email Address"
+                    name="email"
+                    autoComplete="email"
+                    variant="outlined"
+                    onChange={(e) => {setEmail(e.target.value)}}
+                />
+                <TextField
+                    margin="normal"
+                    required
+                    fullWidth
+                    name="password"
+                    label="Password"
+                    type="password"
+                    id="password"
+                    autoComplete="current-password"
+                    variant="outlined"
+                    onChange={(e) => {setPassword(e.target.value)}}
+                />
+                <div>
+                    <InputLabel style={{color:"red"}}>{error}</InputLabel>
+                </div>
+                <Button
+                    type="submit"
+                    fullWidth
+                    variant="contained"
+                    sx={{mt:3, mb: 3 }}
+                >
+                    Sign In
+                </Button>
+                <Stack style={{textAlign:"center"}}>
+                    <Stack>
+                        {"Don't have an account? "}
+                    </Stack>
+                    <Stack>
+                        <Link to="/register" style={{color:"dodgerblue", textDecoration:"None"}}>
+                            {"Sign up"}
+                        </Link>
+                    </Stack>
+                </Stack>
+            </Box>
+        </Box>
+    </>
+}
--- Frontend/src/main.tsx
+++ Frontend/src/main.tsx
@@ -1,10 +1,41 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
+import {createBrowserRouter, Navigate, RouterProvider,} from "react-router-dom"
 import './index.css'
+import {UnauthenticatedRoute} from "./components/UnauthenticatedRoute.tsx";
+import Login from "./pages/Login.tsx";
+import {AuthenticatedRoute} from "./components/AuthenticatedRoute.tsx";
+import Home from "./pages/Home.tsx";
+import {AuthProvider} from "./components/AuthContext.tsx";
+import {createTheme, ThemeProvider} from "@mui/material";
+import axios from "axios";
+
+axios.defaults.withCredentials = true
+
+const theme = createTheme({
+    palette: {
+        primary: {
+            main: "#394867",
+        },
+        secondary: {
+            main: "#9BA4B5",
+            contrastText: 'white'
+        },
+    },
+});
+
+const router = createBrowserRouter([
+    {path:"/login", element: <UnauthenticatedRoute><Login/></UnauthenticatedRoute>},
+    {path:"/home", element: <AuthenticatedRoute><Home/></AuthenticatedRoute>},
+    {path:"*", element: <Navigate to="/home" replace />},
+])
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+      <ThemeProvider theme={theme}>
+          <AuthProvider>
+            <RouterProvider router={router}/>
+          </AuthProvider>
+      </ThemeProvider>
   </React.StrictMode>,
 )
