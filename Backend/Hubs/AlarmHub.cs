649b3f3c6071158cfda6cd472bf9c90fe769b5d3
Jovan Jokić <102528502+jokicjovan@users.noreply.github.com>
2023-07-05 19:26:59 +0200

    Update AlarmHub.cs

--- Backend/Hubs/AlarmHub.cs
+++ Backend/Hubs/AlarmHub.cs
@@ -1,9 +1,11 @@
 ﻿using Brewery_SCADA_System.DTO;
 using Brewery_SCADA_System.Models;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.SignalR;
 
 namespace Brewery_SCADA_System.Hubs
 {
+    [Authorize]
     public class AlarmHub : Hub<IAlarmClient>
     {
         public AlarmHub()
